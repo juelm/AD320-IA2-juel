3b242ebdcc6ef7a1e25ca936d44e513fd090e2af
Matt Juel <mattcjuel@gmail.com>
2019-01-30 23:57:59 -0800

    Resubmitting. Previous files corrupted

---
+++ submit.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <?php
+    
+            echo "Name: {$_GET["name"]}<br>
+                  Email: {$_GET["email"]}<br>
+                  Phone: {$_GET["phone"]}<br>
+                  State: {$_GET["state"]}";
+              
+
+        ?>   
+    </body>
+</html>
